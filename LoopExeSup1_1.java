9ac428ae685d4818902a3c849efa63a062774393
regelo <34382451+regelo@users.noreply.github.com>
2022-04-28 21:01:37 -0400

    Create LoopExeSup1_1.java

---
+++ LoopExeSup1_1.java
@@ -0,0 +1,15 @@
+package loopexesup1_1;
+
+public class LoopExeSup1_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Affichage de cinq messages");
+		System.out.println("Salut le monde !"); // 1
+		System.out.println("Salut le monde !"); // 2
+		System.out.println("Salut le monde !"); // 3
+		System.out.println("Salut le monde !"); // 4
+		System.out.println("Salut le monde !"); // 5
+	}
+
+}
