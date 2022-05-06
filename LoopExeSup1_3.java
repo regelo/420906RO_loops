0b4b3f513b29da2be145bf5b6f406c0715abf6f8
regelo <34382451+regelo@users.noreply.github.com>
2022-05-06 08:18:59 -0400

    Create LoopExeSup1_3.java

---
+++ LoopExeSup1_3.java
@@ -0,0 +1,24 @@
+package loopexesup1_3;
+
+public class LoopExeSup1_3 {
+
+	public static void main(String[] args) {
+		System.out.println("Affichage de valeurs factorielles : ");
+		int factorielle = 1;
+		
+		factorielle = factorielle * 2; 		// Calcul de 2!
+		System.out.println(factorielle);	// Affichage de 2!
+		factorielle = factorielle * 3; 		// Calcul de 3!
+		System.out.println(factorielle);	// Affichage de 3!
+		factorielle = factorielle * 4; 		// Calcul de 4!
+		System.out.println(factorielle);	// Affichage de 4!
+		factorielle = factorielle * 5; 		// Calcul de 5!
+		System.out.println(factorielle);	// Affichage de 5!
+		factorielle = factorielle * 6; 		// Calcul de 6!
+		System.out.println(factorielle);	// Affichage de 6!
+		
+		System.out.println("Fin de l'affichage");
+	}
+
+}
+
