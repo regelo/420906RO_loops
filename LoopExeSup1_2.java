c56f29e5737f51ec59671f55d7efe6d02d8dfb40
regelo <34382451+regelo@users.noreply.github.com>
2022-05-06 07:59:04 -0400

    Create LoopExeSup1_2.java

---
+++ LoopExeSup1_2.java
@@ -0,0 +1,16 @@
+package loopexesup1_2;
+
+public class LoopExeSup1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Affichage de cinq messages");
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
+		System.out.println(8);
+		System.out.println(9);
+	}
+
+}
